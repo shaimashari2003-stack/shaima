27be50f31921bc790ca4b12a8f8eb2c6ccb0a6d0
shaimashari2003-stack <shaimashari2003@gmail.com>
2025-09-10 17:35:05 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function sendMail() {
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+    };
+}
